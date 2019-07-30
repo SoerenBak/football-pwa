689cff8226a77c82a01174086dd6a2ec03a67f04
SoerenBak <34088652+SoerenBak@users.noreply.github.com>
2019-07-30 17:17:27 +0200

    build

--- build/CustomServiceWorker.js
+++ build/CustomServiceWorker.js
@@ -1,4 +1,4 @@
-console.log("Custom Service Worker !");
+console.log("Custom Service Worker!§!#");
 
 self.addEventListener('install', event => {
     console.log('The service worker is being installed.');
@@ -34,15 +34,18 @@ workbox.routing.registerRoute('https://trans-pwa.herokuapp.com/transfers',
   'GET'
 );
 
-workbox.routing.registerRoute(
-  'https://trans-pwa.herokuapp.com/createTrans',
-   new workbox.strategies.networkFirst(),
+workbox.routing.registerRoute('https://trans-pwa.herokuapp.com/getTrans',
+  new workbox.strategies.NetworkFirst(),
+  'GET'
+);
+
+workbox.routing.registerRoute('https://trans-pwa.herokuapp.com/createTrans',
+   new workbox.strategies.NetworkFirst(),
   'POST'
 );
 
-workbox.routing.registerRoute(
-  'https://trans-pwa.herokuapp.com/api/push_message',
-  workbox.strategies.networkFirst(),
+workbox.routing.registerRoute('https://trans-pwa.herokuapp.com/api/push_message',
+  new workbox.strategies.NetworkFirst(),
   'POST'
 );
 
