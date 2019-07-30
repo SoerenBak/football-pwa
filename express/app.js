f77a5d68a2157a235173fa46498e710b533029c9
SoerenBak <34088652+SoerenBak@users.noreply.github.com>
2019-07-30 20:27:48 +0200

    fixed

--- express/app.js
+++ express/app.js
@@ -133,7 +133,6 @@ app.post('/api/subscribe', (req, res) => { // Store subscription on server
 app.post('/api/push_message', (req, res, next) => {
     let text = req.body.text;
     let date = req.body.date;
-    let img = './images/site/logo.png'
 
     Subscriptions.find({}, (err, sub) => {
         if (err) {
@@ -142,8 +141,7 @@ app.post('/api/push_message', (req, res, next) => {
         sub.forEach((elm) => {
             const payload = JSON.stringify({
                 msg: date,
-                text: text,
-                img: img
+                text: text
             });
 
             webpush.sendNotification(elm, payload).catch(error => {
